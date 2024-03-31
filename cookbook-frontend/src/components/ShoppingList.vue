d01cd49371a7adcb9af582d4906a52f89a103876
Doug Schultz <douglasschultz1@gmail.com>
2024-03-30 22:33:04 -0400

    fix shopping list

--- cookbook-frontend/src/components/ShoppingList.vue
+++ cookbook-frontend/src/components/ShoppingList.vue
@@ -2,17 +2,20 @@
   <v-container id="shopping-list">
     <h2>Shopping List</h2>
     <v-textarea
-      :model-value="$props.shoppingList"
+      v-model="list"
       rows="15"
     ></v-textarea>
   </v-container>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
 
-defineProps<{
+const props = defineProps<{
   shoppingList:string
 }>()
+
+const list = ref(props.shoppingList)
 </script>
 
 <style scoped>
